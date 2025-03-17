7bd22f120cc254b427976777f9b4a381b618e1d4
Selim Fekih <selimfek@mgmail.com>
2025-03-17 11:46:03 +0100

    fix images description pipeline, add ocr extraction

--- documents_processing/documents_data_extraction.py
+++ documents_processing/documents_data_extraction.py
@@ -21,6 +21,9 @@ from documents_processing.utils import (
 from documents_processing.prompts import system_prompts, metadata_extraction_prompt
 from llm_multiprocessing_inference import get_answers
 from PIL import Image
+import pytesseract
+import io
+from tqdm import tqdm
 
 inference_pipelines = {
     "OpenAI": {
@@ -34,6 +37,34 @@ inference_pipelines = {
         "api_key": None,
     },
 }
+
+
+def _ocr_handwritten_pdf(file_path) -> List[str]:
+    """
+    Opens a PDF file and initializes an empty list to hold the OCR-extracted text.
+    Iterates through each page of the PDF, rendering it to an image.
+    Uses Tesseract OCR, via pytesseract.image_to_string, to extract text from the image of each page.
+    Appends the extracted text to the list and closes the PDF document after processing all pages.
+    Returns the list of extracted text blocks.
+    """
+    # Open the PDF
+    doc = fitz.open(file_path)
+    text = []
+
+    for page_num in range(len(doc)):
+        # Get the page
+        page = doc.load_page(page_num)
+
+        # Render page to an image
+        pix = page.get_pixmap()
+        image_bytes = pix.tobytes("png")
+        image = Image.open(io.BytesIO(image_bytes))
+
+        # Use Tesseract to do OCR on the image
+        text.append(pytesseract.image_to_string(image))
+
+    doc.close()
+    return text
 
 
 class DocumentsDataExtractor:
@@ -118,16 +149,16 @@ class DocumentsDataExtractor:
                         {"role": "system", "content": system_prompts[fig_type]},
                         {
                             "role": "user",
-                            "images": [fig_path],
-                        }
+                            "images": [base64_image],
+                        },
                     ]
                 prompts.append(one_fig_prompt)
 
         if len(prompts) > 0:
             answers = get_answers(
                 prompts=prompts,
-                default_response={},
-                response_type="structured",
+                default_response="-",
+                response_type="unstructured",
                 api_pipeline=self.inference_pipeline_name,
                 model=self.model_name,
                 api_key=self.api_key,
@@ -150,7 +181,7 @@ class DocumentsDataExtractor:
             base64_image = encode_image(one_page_path)
 
             if self.inference_pipeline_name == "OpenAI":  # OpenAI
-                
+
                 prompt = [
                     {
                         "role": "user",
@@ -218,7 +249,6 @@ class DocumentsDataExtractor:
         # doc_folder_path = os.path.abspath(doc_folder_path)
         doc_file_path = os.path.join(doc_folder_path, file_name)
 
-
         if not os.path.exists(doc_file_path):
             _download_pdf(doc_url, doc_file_path)
 
@@ -229,8 +259,20 @@ class DocumentsDataExtractor:
             doc_file_path = converted_pdf_path
 
         extracted_text = self._extract_pdf_text(doc_file_path)
+        extracted_ocr_text = _ocr_handwritten_pdf(doc_file_path)
         df_raw_text = pd.DataFrame(
-            [{"text": extracted_text, "Entry Type": "PDF Text", "entry_fig_path": "-"}]
+            [
+                {
+                    "text": extracted_text,
+                    "Entry Type": "PDF Text",
+                    "entry_fig_path": "-",
+                },
+                {
+                    "text": extracted_ocr_text,
+                    "Entry Type": "PDF OCR Text",
+                    "entry_fig_path": "-",
+                },
+            ]
         )
 
         project_extracted_text = pd.concat([project_extracted_text, df_raw_text])
